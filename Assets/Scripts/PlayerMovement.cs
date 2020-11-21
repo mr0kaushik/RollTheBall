0fbf51cbdb22d575607d167a4d3ec16257cb5a6f
Deepak Kaushik <kaushikd875@gmail.com>
2020-11-22 01:05:35 +0530

    Mobile Touch Input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@
 public class PlayerMovement : MonoBehaviour
 {
 
+    
     private float m_YAxisGameOverPoint = 0f;
 
     [SerializeField]
@@ -20,7 +21,49 @@ public class PlayerMovement : MonoBehaviour
         m_GameManager = FindObjectOfType<GameManager>();
     }
 
-    void FixedUpdate()
+
+    void Update()
+    {
+        
+        rb.AddForce(0, 0, m_ForwardForce * Time.deltaTime);
+
+        Debug.Log("Touch Count " + Input.touchCount);
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Debug.Log("Touch Position : " + touch.position.ToString());
+
+            Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            Debug.Log("Touch With Camera Position : " + pos.ToString());
+            
+            Debug.Log("Rigid Body Positoin " + rb.position.ToString());
+            Debug.Log("Value " + (Vector3.right * pos.x * Time.deltaTime * m_Speed).ToString());
+
+            
+            if( pos.x > 0){
+    
+                rb.MovePosition(rb.position + (Vector3.right * pos.x * Time.deltaTime * m_Speed));
+                // rb.position + (Vector3.right * touchPosition.x * Time.deltaTime * m_Speed)                
+            } else {
+                rb.MovePosition(rb.position + (Vector3.right * pos.x * Time.deltaTime * m_Speed));
+            }
+
+            if (rb.position.y < m_YAxisGameOverPoint)
+        {
+            m_GameManager.GameOver();
+        }
+
+            //     rb.MovePosition();
+            // } else {
+            //     rb.MovePosition();
+            // }
+        }
+    }
+
+    /* void FixedUpdate()
     {
 
 
@@ -38,6 +81,6 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
-
+ */
 
 }
